c645cf39f87f1df86d2eb9f5786a78e82d5a06e7
Brendan Gooch <brendandevelopment936@gmail.com>
2021-12-01 12:41:29 +0000

    initial commit

---
+++ js/playingCard.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+class PlayingCard {
+    #symbol = false;
+    constructor() {}
+    get symbol() {}
+    get suit() {}
+    get value() {}
+    get fullName() {}
+    isJoker() {}
+}
